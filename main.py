8ae79fef6fec186fe39b7590670b8cb70e5d1c2d
Sannidhya <shubhradasgupta8@gmail.com>
2021-05-31 00:05:45 +0530

    Started this new mini project, Contact Manager. made the starting and the functions. The main functions logiscs will be made later. I started the project in midnight, so can't continue much now

---
+++ main.py
@@ -0,0 +1,16 @@
+# This programs belongs to Mr. Stupidity x 100
+
+import keyword
+
+
+
+
+
+if __name__ == '__main__':
+    print("\n\n***************Contact Manager at your service sir/madam!!!!***************\n\n")
+
+    Instructions = "To view all contacts hit 'A' on your keyboard\n\nTo add a new contact hit 'T' on your keyboard\n\nTo delete a contact hit 'D' on the keyboard\n\n(For Devs)Type 'src' to get the source code the program.\n\nTo be the redirected to the Github repo type 'gh redir'.\n\n"
+
+    print(Instructions)
+
+    UserInput = input("Enter here: ")
