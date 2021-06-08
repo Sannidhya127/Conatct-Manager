ca5c447341c34d8b9186d0fc450559fd58aecb0b
Sannidhya <shubhradasgupta8@gmail.com>
2021-06-08 17:07:20 +0530

    Completed the add function, list all function and added an exit command. The add contact function is now much better than before. The input procedure also works in a while loop now

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 # This programs belongs to Mr. Stupidity x 100
 
 import keyword
+from typing import MutableSequence
 from typing_extensions import ParamSpecKwargs
 from datetime import date
+import string
+from colored import fg, bg, attr
+
 
 def addContact():
-    contact = input("Enter the mobile number: ")
-    ContactName = input("Enter the contact name: ")
-    ContactsFileCreationOrWriting = open("contacts.txt", "a")
-    FileContent = ContactsFileCreationOrWriting.write(f"\n{date.today()} | {contact} | {ContactName}\n")
+    try:
+        contact = input("Enter the mobile number: ")
+        ContactName = input("Enter the contact name: ")
+        ContactsFileCreationOrWriting = open("contacts.txt", "a")
+        confirmation = input(f"Contact Name: {ContactName}\nContact Number: {contact}\nConfirm credentials? [Y/N]: ")
+        EndConfirmation = confirmation.lower()
+        for i in string.ascii_lowercase:
+            if i in contact:
+                print("Invalid Number. Contains alphabets")
+            elif i not in contact:
+                if EndConfirmation == "y":
+                    FileContent = ContactsFileCreationOrWriting.write(f"\n{date.today()} | {contact} | {ContactName}\n")
+                elif EndConfirmation == "n":
+                    addContact()
+                else:
+                    print("Failed loading script main")
+            else:
+                print("Failed loading script main")
+    except Exception as e:
+        print(e)
 
 
 def deleteContact():
     pass
 
 def listAllContact():
-    pass
+    file = open("contacts.txt", "r")
+    FileContent = file.read()
+    print(FileContent)
 
 def SourceCode():
     pass
@@ -31,22 +53,27 @@ if __name__ == '__main__':
     Instructions = "To view all contacts hit 'A' on your keyboard\n\nTo add a new contact hit 'T' on your keyboard\n\nTo delete a contact hit 'D' on the keyboard\n\n(For Devs)Type 'src' to get the source code the program.\n\nTo be the redirected to the Github repo type 'gh redir'.\n\n"
 
     print(Instructions)
+    while True:
+        UserInput = input(f"{fg('chartreuse_2a')}Enter here: {attr('reset')}")
+        MainInput = UserInput.lower()
 
-    UserInput = input("Enter here: ")
-    MainInput = UserInput.lower()
 
+        if MainInput == "a":
+            listAllContact()
 
-    if MainInput == "a":
-        listAllContact()
+        elif MainInput == "t":
+            addContact()
 
-    elif MainInput == "t":
-        addContact()
+        elif MainInput == "d":
+            deleteContact()
 
-    elif MainInput == "d":
-        deleteContact()
+        elif MainInput == "src":
+            SourceCode()
 
-    elif MainInput == "src":
-        SourceCode()
-
-    elif MainInput == "gh redir":
-        GithubRedirection()
+        elif MainInput == "gh redir":
+            GithubRedirection()
+        
+        elif MainInput == "exit":
+            exit()
+        elif MainInput == "quit":
+            exit()
